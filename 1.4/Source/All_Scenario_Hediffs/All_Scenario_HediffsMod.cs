d76a9e705cf1b13e436f68624a002e02c62686e8
Dexter Lowe <feldoh10@gmail.com>
2024-02-13 00:10:46 +0000

    Cookie cutter initial commit

---
+++ 1.4/Source/All_Scenario_Hediffs/All_Scenario_HediffsMod.cs
@@ -0,0 +1,29 @@
+﻿using Verse;
+using UnityEngine;
+
+namespace All_Scenario_Hediffs;
+
+public class All_Scenario_HediffsMod : Verse.Mod
+{
+    public static Settings settings;
+
+    public All_Scenario_HediffsMod(ModContentPack content) : base(content)
+    {
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+
+
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "All Scenario Hediffs";
+    }
+}
