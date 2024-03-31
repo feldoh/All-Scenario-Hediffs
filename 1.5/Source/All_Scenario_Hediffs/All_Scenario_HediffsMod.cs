c3373bc6c5549d30c10de1f5e12b7f8074a05c36
Dexter Lowe <feldoh10@gmail.com>
2024-03-31 20:28:57 +0100

    1.5

---
+++ 1.5/Source/All_Scenario_Hediffs/All_Scenario_HediffsMod.cs
@@ -0,0 +1,36 @@
+﻿using Verse;
+using UnityEngine;
+
+namespace All_Scenario_Hediffs;
+
+[StaticConstructorOnStartup]
+public static class ApplyOverrides
+{
+    static ApplyOverrides()
+    {
+        All_Scenario_HediffsMod.settings.ApplyOverrides();
+    }
+}
+
+public class All_Scenario_HediffsMod : Mod
+{
+    public static Settings settings;
+
+    public All_Scenario_HediffsMod(ModContentPack content) : base(content)
+    {
+        // initialize settings
+        settings = GetSettings<Settings>();
+        settings.ApplyOverrides();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "Feldoh_AllScenarioHediffs_SettingsName".Translate();
+    }
+}
